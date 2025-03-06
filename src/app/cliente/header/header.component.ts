b642c6522ba445ce633e15cac723156311a1359b
drodriguez5503 <davidrdelope@gmail.com>
2025-03-06 18:05:18 +0100

    Cambios en el header cliente para tener en cuenta sesion iniciada

--- src/app/cliente/header/header.component.ts
+++ src/app/cliente/header/header.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgIf, NgOptimizedImage} from "@angular/common";
 import {LoginComponent} from '../login/login.component';
 import {RouterLink} from '@angular/router';
+import {TokenService} from '../../services/auth/token.service';
+import {TabUserComponent} from '../../backoffice/tabs/tab-user/tab-user.component';
 
 @Component({
   selector: 'app-header',
   imports: [
     NgOptimizedImage,
-    RouterLink
+    RouterLink,
+    NgIf,
+    TabUserComponent
   ],
   templateUrl: './header.component.html',
   standalone: true,
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  token: string | null = null;
+  userActive: boolean = false;
+
+  constructor(private tokenService: TokenService) {
+  }
+
+   ngOnInit() {
+    this.token = this.tokenService.getAccessToken();
+    console.log(this.token);
+   }
+
+   toggleItem() {
+    this.userActive = !this.userActive;
+   }
 
 }

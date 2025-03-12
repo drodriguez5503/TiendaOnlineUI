366c5960b7b63e598f8354fb8444e8e237722d1e
drodriguez5503 <davidrdelope@gmail.com>
2025-03-12 13:59:55 +0100

    Maquetación inicial componente perfil

--- src/app/backoffice/profile/profile.component.ts
+++ src/app/backoffice/profile/profile.component.ts
@@ -1,33 +1,59 @@
-import {Component, ElementRef, HostListener, OnInit, ViewChild} from '@angular/core';
-import {NgIf} from '@angular/common';
-import {ProfileModalComponent} from '../profile-modal/profile-modal.component';
-import {UserTabComponent} from '../profile-tabs/user-tab/user-tab.component';
+import {Component} from '@angular/core';
+import {NgForOf, NgIf} from '@angular/common';
+import Swal from 'sweetalert2';
+import {ToastrService} from 'ngx-toastr';
+import {Product} from '../../services/interfaces/product';
 
 @Component({
   selector: 'app-profile',
   imports: [
     NgIf,
-    ProfileModalComponent,
-    UserTabComponent
+    NgForOf
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-  ActiveUser: boolean = false;
-  isSearchModalOpen:boolean = false;
+  products : Product[] = [];
 
+  constructor(private toastr: ToastrService) {}
 
-  showSearchModal():void {
-    this.isSearchModalOpen = true;
+  addProduct() {
+    Swal.fire({
+      title: 'Agregar Producto',
+      html: `<input id="productName" class="swal2-input" placeholder="Nombre del Producto">
+             <input id="productPrice" type="number" class="swal2-input" placeholder="Precio">`,
+      showCancelButton: true,
+      confirmButtonText: 'Agregar',
+      preConfirm: () => {
+        const name = (document.getElementById('productName') as HTMLInputElement).value;
+        const price = +(document.getElementById('productPrice') as HTMLInputElement).value;
+        if (!name || !price) {
+          Swal.showValidationMessage('Ambos campos son obligatorios');
+        }
+        return { name, price };
+      }
+    }).then((result) => {
+      if (result.isConfirmed && result.value) {
+        this.products.push(result.value);
+        this.toastr.success('Producto agregado con éxito', 'Éxito');
+      }
+    });
   }
 
-  hideSearchModal():void {
-      this.isSearchModalOpen = false;
+  removeProduct(index: number) {
+    Swal.fire({
+      title: '¿Eliminar producto?',
+      text: 'Esta acción no se puede deshacer',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.products.splice(index, 1);
+        this.toastr.info('Producto eliminado', 'Eliminado');
+      }
+    });
   }
-
-  toggleUser():void {
-    this.ActiveUser = !this.ActiveUser;
-  }
-
 }
